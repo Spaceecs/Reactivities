9b8b4e6da422342b4d41b6190ba2e1c0a0b01d2d
Spaceecs <bocianovski.ol@e-u.edu.ua>
2026-06-15 02:45:51 +0300

    password reset complete

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -4,10 +4,20 @@ import type { LoginSchema } from "../schemas/loginSchema.ts";
 import { useNavigate } from "react-router";
 import type { RegisterSchema } from "../schemas/registerSchema.ts";
 import { toast } from "react-toastify";
+import type { ChangePasswordSchema } from "../schemas/changePasswordSchema.ts";
 
 export const useAccount = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
+  const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await agent.get<User>("/account/user-info");
+      return response.data;
+    },
+    enabled: !queryClient.getQueryData(["user"]),
+  });
 
   const loginUser = useMutation({
     mutationFn: async (creds: LoginSchema) => {
@@ -61,13 +71,22 @@ export const useAccount = () => {
     },
   });
 
-  const { data: currentUser, isLoading: loadingUserInfo } = useQuery({
-    queryKey: ["user"],
-    queryFn: async () => {
-      const response = await agent.get<User>("/account/user-info");
-      return response.data;
+  const changePassword = useMutation({
+    mutationFn: async (data: ChangePasswordSchema) => {
+      await agent.post("/account/change-password", data);
     },
-    enabled: !queryClient.getQueryData(["user"]),
+  });
+
+  const forgotPassword = useMutation({
+    mutationFn: async (email: string) => {
+      await agent.post("/forgotPassword", { email });
+    },
+  });
+
+  const resetPassword = useMutation({
+    mutationFn: async (data: ResetPassword) => {
+      await agent.post("/resetPassword", data);
+    },
   });
 
   return {
@@ -78,5 +97,8 @@ export const useAccount = () => {
     registerUser,
     resendConfirmationEmail,
     verifyEmail,
+    changePassword,
+    forgotPassword,
+    resetPassword,
   };
 };
